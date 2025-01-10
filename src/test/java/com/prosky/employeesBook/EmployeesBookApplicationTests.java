e3d99c22f9bf3ccfe0d94ceb0e68a6d886a24319
Ваня <brazik1031@icloud.com>
2025-01-10 14:02:11 +0300

    Start homework

---
+++ src/test/java/com/prosky/employeesBook/EmployeesBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prosky.employeesBook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeesBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
